73c6e06f942e5fc17b472b50eb94d8b7807d5fb9
iamAAC <38164424+iamaac7@users.noreply.github.com>
2018-11-23 21:22:35 +0600

    updated view.php
    all working now

--- FL2/view.php
+++ FL2/view.php
@@ -1,22 +1,18 @@
 <?php
 
 $usr= $_POST ['name'];
-$fullname= $_POST ['address'];
-$email= $_POST ['zipcode'];
-$phone= $_POST ['country'];
-$cpass= $_POST ['phone'];
-$gen= $_POST ['email'];
+$phone= $_POST ['phone'];
+$email= $_POST ['email'];
 $pass= $_POST ['password'];
 $chk= $_POST ['pwVerified'];
 
 echo "</br>Registration Sucessful !";
 echo "</br>Your user name is : ".$usr;
-echo "</br>Your Full Name : ".$fullname;
 echo "</br>Your Email: ".$email;
 echo "</br>Your phone : ".$phone;
 echo "</br>Your password: ".$pass;
 echo "</br>Your password: ".$chk;
-echo "</br>Your gender: ".$gen;
+
 
 
 ?>
